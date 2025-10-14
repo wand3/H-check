8f718641f15114c63febafd2bc0644deadfd849c
wand3 <godswillbaiyewu@gmail.com>
2025-10-14 06:16:39 +0100

    frontent layout success

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -15,14 +15,13 @@ const HBody: FC<BodyProps> = ({ nav, children }) => {
     {/* dark:bg-[#F4F2F0] */}
       {/* <div className="inset-0 radial-blur top-[-15%] sm:top-[-10%] h-[115vh] sm:h-[80vh] md:h-[120vh] mx-[5%] rounded-lg lg:mt-0 lg:h-[96vh] left-1/3 w-[96%] sm:w-[100vw] -translate-x-1/2 overflow-hidden fixed"></div> */}
 
-      <div className="bg-[#e7f0c6] dark:bg-[#000000] p-5">
+      <div className="bg-[#e7f0c6] p-5">
         {/* <div className="font-sans bg-gray-500 bg-clip-padding backdrop-filter  backdrop-blur bg-opacity-10 backdrop-saturate-100 backdrop-contrast-100"> */}
 
         {/* <div className="font-sans"> */}
 
 
           {children}
-          <h1>Test to see</h1>
         {/* </div> */}
         
       </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,13 @@ const Home = () => {
         <> 
             <HBody nav={false}>
               <h1 className='text-lg text-center text-white'>Homepage success</h1>
+              <div id="chat-container">
+                    <div id="messages"></div>
+                    <form id="message-form">
+                    <input type="text" id="message-input" placeholder="Type your message" autocomplete="off" />
+                    <button type="submit">Send</button>
+                    </form>
+                </div>
             </HBody>
         </>
     )
